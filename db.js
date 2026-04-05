2f2b3af4ef0d9263eb3fd76bcea3cbed29e73cff
wellingtonxxp-a11y <wellingtonxxp@gmail.com>
2026-04-05 09:49:51 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,10 +1,26 @@
 const { Pool } = require("pg");
 
+const connectionString = process.env.DATABASE_URL;
+
+if (!connectionString) {
+  console.error("DATABASE_URL não definido");
+  process.exit(1);
+}
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString,
   ssl: {
     rejectUnauthorized: false,
   },
+});
+
+pool.on("connect", () => {
+  console.log("Conectado ao PostgreSQL");
+});
+
+pool.on("error", (err) => {
+  console.error("Erro no pool:", err);
+  process.exit(1);
 });
 
 module.exports = pool;
