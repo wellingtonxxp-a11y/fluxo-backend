bc196a830001a73855d49331b36d10fdd426bf1b
wellingtonxxp-a11y <wellingtonxxp@gmail.com>
2026-04-05 10:24:18 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 
+console.log("DATABASE_URL:", process.env.DATABASE_URL);
 const express = require("express");
 const cors = require("cors");
 const bcrypt = require("bcrypt");
