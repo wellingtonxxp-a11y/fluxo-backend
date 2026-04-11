0d986619e34dea97a1b2cf9732a9a7395eab1d52
wellingtonxxp-a11y <wellingtonxxp@gmail.com>
2026-04-11 14:04:58 -0300

    refactor: backend prisma

--- src/config/prisma.js
+++ src/config/prisma.js
@@ -1,18 +1,5 @@
 const { PrismaClient } = require("@prisma/client");
-const { Pool } = require("pg");
-const { PrismaPg } = require("@prisma/adapter-pg");
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
-
-const adapter = new PrismaPg(pool);
-
-const prisma = new PrismaClient({
-  adapter,
-});
+const prisma = new PrismaClient();
 
 module.exports = prisma;
