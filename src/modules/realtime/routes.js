7fbf31e841fb1f15da23695fc546f5c32c2e8d12
wellingtonxxp-a11y <wellingtonxxp@gmail.com>
2026-04-12 09:55:06 -0300

    refactor: multi

--- src/modules/realtime/routes.js
+++ src/modules/realtime/routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const auth = require("../../middleware/auth");
+const auth = require("../../middlewares/auth");
 const { getRealtimeRecommendation } = require("./service");
 
 router.get("/copilot", auth, async (req, res) => {
--- src/modules/realtime/service.js
+++ src/modules/realtime/service.js
@@ -1,4 +1,4 @@
-const prisma = require("../../config/prisma");
+const prisma = require("../../../prisma");
 
 async function getRealtimeRecommendation(userId) {
 
