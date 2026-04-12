db738a0326590939544f9f78f5e0113ffd2ce5f3
wellingtonxxp-a11y <wellingtonxxp@gmail.com>
2026-04-11 21:36:48 -0300

    Update prisma.config.js

--- prisma.config.js
+++ prisma.config.js
@@ -1,10 +1,8 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
-  schema: './prisma/schema.prisma',
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    },
+  schema: "./prisma/schema.prisma",
+  datasource: {
+    url: process.env.DATABASE_URL,
   },
 };
